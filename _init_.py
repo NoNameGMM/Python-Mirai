7e523bdc4e445415f209f3a92995818f6a642d97
NoNameGMM <gmm0119@163.com>
2024-03-10 20:40:52 +0800

    Update _init_.py

--- _init_.py
+++ _init_.py
@@ -10,17 +10,7 @@ current_directory = os.path.dirname(os.path.realpath(__file__))
 
 new_folder_path = current_directory + '/libs'
 os.mkdir(new_folder_path)
-
-jython_url = 'https://cdn.nonamegmm.tk/jython/windows/jython.zip'
-jdk_url = 'https://mirrors.tuna.tsinghua.edu.cn/Adoptium/17/jdk/x32/windows/OpenJDK17U-jdk_x86-32_windows_hotspot_17.0.10_7.zip'
-mirai_url = 'https://maven.aliyun.com/repository/central/net/mamoe/mirai-core-all/2.16.0/mirai-core-all-2.16.0-all.jar'
-jython_file = "jython.zip"
-jdk_file = "OpenJDK17U-jdk_x86-32_windows_hotspot_17.0.10_7.zip"
-mirai_folder = current_directory + "/libs"
-mirai_file = mirai_folder + "/mirai-core-all-2.16.0-all.jar"
-jython_extract_folder = current_directory + "/jython"
-jdk_extract_folder = current_directory
-
+block_size = 1024 
 
 def rename_folder(old_folder_name, new_folder_name):
     try:
@@ -32,43 +22,62 @@ def rename_folder(old_folder_name, new_folder_name):
 old_folder_name = current_directory + "/jdk-17.0.10+7"
 new_folder_name = current_directory + '/jdk'
 
+def install_jython():
+    jython_url = 'https://cdn.nonamegmm.tk/jython/windows/jython.zip'
+    jython_file = "jython.zip"
+    jython_extract_folder = current_directory + "/jython"
+    response = requests.get(jython_url, stream=True)
+    total_size = int(response.headers.get('content-length', 0))
+    with open(jython_file, 'wb') as file:
+        for data in tqdm(response.iter_content(block_size), total=total_size//block_size, unit='KB', unit_scale=False):
+            file.write(data)
+    with zipfile.ZipFile(jython_file, 'r') as zip_ref:
+        zip_ref.extractall(jython_extract_folder)
+        response.close()
+    os.remove(jython_file)
+    print("---文件解压成功---")
+    print("---下载文件删除成功---")
+
+def install_jdk():
+    jdk_url = 'https://mirrors.tuna.tsinghua.edu.cn/Adoptium/17/jdk/x32/windows/OpenJDK17U-jdk_x86-32_windows_hotspot_17.0.10_7.zip'
+    jdk_file = "OpenJDK17U-jdk_x86-32_windows_hotspot_17.0.10_7.zip"
+    jdk_extract_folder = current_directory
+    response = requests.get(jdk_url, stream=True)
+    total_size1 = int(response.headers.get('content-length', 0))
+    with open(jdk_file, 'wb') as file:
+        for data in tqdm(response.iter_content(block_size), total=total_size1//block_size, unit='KB', unit_scale=False):
+            file.write(data)
+    with zipfile.ZipFile(jdk_file, 'r') as zip_ref:
+        zip_ref.extractall(jdk_extract_folder)
+        response.close()
+    os.remove(jdk_file)
+    rename_folder(old_folder_name, new_folder_name)
+    print("---文件解压成功---")
+    print("---下载文件删除成功---")
+    
+def install_mirai():
+    mirai_url = 'https://repo1.maven.org/maven2/net/mamoe/mirai-core-all/2.16.0/mirai-core-all-2.16.0-all.jar'
+    mirai_folder = current_directory + "/libs"
+    mirai_file = mirai_folder + "/mirai-core-all-2.16.0-all.jar"
+    response = requests.get(mirai_url, stream=True)
+    total_size = int(response.headers.get('content-length', 0))
+    with open(mirai_file, 'wb') as file:
+        for data in tqdm(response.iter_content(block_size), total=total_size//block_size, unit='KB', unit_scale=False):
+            file.write(data)
 
 print("---开始下载依赖---")
-
-response = requests.get(jython_url, stream=True)
-response1 = requests.get(jdk_url, stream=True)
-response2 = requests.get(mirai_url, stream=True)
-
-total_size = int(response.headers.get('content-length', 0))
-total_size1 = int(response1.headers.get('content-length', 0))
-total_size2 = int(response2.headers.get('content-length', 0))
-block_size = 1024 
-
-with open(jython_file, 'wb') as file:
-    for data in tqdm(response.iter_content(block_size), total=total_size//block_size, unit='KB', unit_scale=False):
-        file.write(data)
-with open(jdk_file, 'wb') as file:
-    for data in tqdm(response1.iter_content(block_size), total=total_size1//block_size, unit='KB', unit_scale=False):
-        file.write(data)
-with open(mirai_file, 'wb') as file:
-    for data in tqdm(response2.iter_content(block_size), total=total_size2//block_size, unit='KB', unit_scale=False):
-        file.write(data)
-
-with zipfile.ZipFile(jython_file, 'r') as zip_ref:
-    zip_ref.extractall(jython_extract_folder)
-    response.close()
-    
-    
-with zipfile.ZipFile(jdk_file, 'r') as zip_ref:
-    zip_ref.extractall(jdk_extract_folder)
-    response1.close()
-
-rename_folder(old_folder_name, new_folder_name)
-
-os.remove(jdk_file)
-os.remove(jython_file)
-print("---文件解压成功---")
-print("---下载文件删除成功---")
+if os.path.exists(current_directory + "/jython"):
+    print("依赖项 Jython 已安装")
+    if os.path.exists(current_directory + "/jdk"):
+        print("依赖项 JDK17 已安装")
+        if os.path.exists(current_directory + "/libs/mirai-core-all-2.16.0-all.jar"):
+            print("依赖项 Mirai 已安装")
+        else:
+            install_mirai()
+    else:
+        install_jdk()
+else:
+    install_jython()
 
 os.environ['JAVA_HOME'] = current_directory + '/jdk'
 
